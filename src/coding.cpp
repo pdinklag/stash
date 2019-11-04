f50a777712bf9fe9b08bd560651844b11d07b123
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2019-11-04 10:20:38 +0100

    Update coding benchmark

--- src/coding.cpp
+++ src/coding.cpp
@@ -3,6 +3,8 @@
 #include <streambuf>
 #include <sstream>
 
+#include <tlx/cmdline_parser.hpp>
+
 #include <code/ASCII.hpp>
 #include <code/Binary.hpp>
 #include <code/Delta.hpp>
@@ -15,28 +17,71 @@
 #include <huff/Forward.hpp>
 #include <huff/Hybrid.hpp>
 
+using mtf_coder_t = MTFCoder<ASCIICoder, ASCIICoder, BinaryCoder<>>;
+
 template<typename coder_t>
-void test(const std::string& input, const std::string& filename) {
+void test(const std::string& input, const std::string& filename, bool verify) {
     std::cout << "# " << filename << " ..." << std::endl;
+    
+    // encode using MTF first
     size_t bits_written;
     {
-        std::ofstream f(filename);
-        BitOStream out(f);
+        std::string mtf_code;
+        {
+            std::ostringstream mtf_ss;
+            BitOStream out(mtf_ss);
+            std::cout << "# encoding with MTF ..." << std::endl;
+            
+            encode<mtf_coder_t>(input, out);
+            bits_written = out.bits_written();
+            mtf_code = mtf_ss.str();
+        }
+        
+        // encode using given coder
+        {
+            std::ofstream f(filename);
+            BitOStream out(f);
 
-        std::cout << "# encoding ..." << std::endl;
-        encode<coder_t>(input, out);
-        bits_written = out.bits_written();
+            std::cout << "# encoding file ..." << std::endl;
+            encode<coder_t>(mtf_code, out);
+            bits_written = out.bits_written();
+        }
     }
+
     bool success;
-    {
-        std::ifstream f(filename);
-        BitIStream in(f);
+    if(verify) {
+        // decode using given coder
+        {
+            std::string mtf_dec;
+            {
+                std::ifstream f(filename);
+                BitIStream in(f);
 
-        std::cout << "# decoding ..." << std::endl;
-        auto dec = decode<coder_t>(in);
+                std::cout << "# decoding file ..." << std::endl;
+                mtf_dec = decode<coder_t>(in);
+            }
 
-        std::cout << "# checking ..." << std::endl;
-        success = (dec == input);
+            // decode MTF
+            std::string dec;
+            {
+                std::istringstream ss(mtf_dec);
+                BitIStream in(ss);
+
+                std::cout << "# decoding with MTF ..." << std::endl;
+                dec = decode<mtf_coder_t>(in);
+            }
+
+            std::cout << "# checking ..." << std::endl;
+            success = (dec == input);
+
+            if(!success) {
+                std::cerr << "# verification failed" << std::endl;
+                std::cerr << "#     input: " << input << std::endl;
+                std::cerr << "#    output: " << dec << std::endl;
+            }
+        }
+    } else {
+        success = true;
     }
 
     std::cout << "RESULT algo=" << filename
@@ -48,24 +93,32 @@ void test(const std::string& input, const std::string& filename) {
 }
 
 int main(int argc, char** argv) {
+    tlx::CmdlineParser cp;
+
+    std::string input_filename;
+    cp.add_param_string("file", input_filename, "The input file.");
+
+    bool verify = false;
+    cp.add_flag("verify", verify, "Decode and verify.");
+
+    if (!cp.process(argc, argv)) {
+        return -1;
+    }
+    
     std::string text;
-    if(argc >= 2) {
-        std::ifstream f(argv[1]);
+    {
+        std::ifstream f(input_filename);
         f.seekg(0, std::ios::end);   
         text.reserve(f.tellg());
         f.seekg(0, std::ios::beg);
 
         text.assign((std::istreambuf_iterator<char>(f)),
                      std::istreambuf_iterator<char>());
-    } else {
-        //text = "bananas&ananas";
-        text = "ss&bnnnn$aaaaaa";
     }
     
-    test<ASCIICoder>(text, "ascii");
-    test<huff::Huffman52Coder<ASCIICoder, DeltaCoder>>(text, "huff52");
-    test<huff::Knuth85Coder<ASCIICoder>>(text, "knuth85");
-    test<huff::ForwardCoder<ASCIICoder, DeltaCoder>>(text, "fwd");
-    test<huff::HybridCoder<ASCIICoder, DeltaCoder>>(text, "hybrid");
-    test<MTFCoder<Delta0Coder, ASCIICoder, DeltaCoder>>(text, "mtf_delta");
+    test<ASCIICoder>(text, "ascii", verify);
+    test<huff::Huffman52Coder<ASCIICoder, DeltaCoder>>(text, "huff52", verify);
+    test<huff::Knuth85Coder<ASCIICoder>>(text, "knuth85", verify);
+    test<huff::ForwardCoder<ASCIICoder, DeltaCoder>>(text, "fwd", verify);
+    test<huff::HybridCoder<ASCIICoder, DeltaCoder>>(text, "hybrid", verify);
 }
