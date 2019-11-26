d4f44fc05c1bb83114ab5103c11c660105e9e7cf
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2019-11-26 10:51:26 +0100

    Allow input and output type of load_file_as_vector to differ

--- include/stash/io/load_file.hpp
+++ include/stash/io/load_file.hpp
@@ -18,30 +18,30 @@ std::string load_file_as_string(const std::string& filename) {
     return s;
 }
 
-template<typename T>
-std::vector<T> load_file_as_vector(
+template<typename I, typename O = I>
+std::vector<O> load_file_as_vector(
     const std::string& filename,
     const size_t bufsize = 16384ULL) {
 
-    std::vector<T> v;
+    std::vector<O> v;
 
     std::ifstream f(filename);
     f.seekg(0, std::ios::end);   
     const size_t sz = f.tellg();
     f.seekg(0, std::ios::beg);
 
-    const size_t total = sz / sizeof(T);
+    const size_t total = sz / sizeof(I);
     v.reserve(total);
 
-    T* buffer = new T[bufsize];
+    I* buffer = new I[bufsize];
     
     size_t left = total;
     while(left) {
         const size_t num = std::min(bufsize, left);
-        f.read((char*)buffer, num * sizeof(T));
+        f.read((char*)buffer, num * sizeof(I));
 
         for(size_t i = 0; i < num; i++) {
-            v.push_back(buffer[i]);
+            v.push_back(O(buffer[i]));
         }
 
         left -= num;
