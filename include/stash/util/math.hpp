65f53b81e016db2e773a6ba96af0e83bb7009332
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2020-02-10 09:36:20 +0100

    Naive is_prime check

--- include/stash/util/math.hpp
+++ include/stash/util/math.hpp
@@ -19,4 +19,30 @@ inline constexpr uint64_t log2_floor(uint64_t x) {
     return 64ULL - __builtin_clzll(x) - 1ULL;
 }
 
+// tests whether p is a prime
+// TODO: use wheel factorization, this is too slow
+inline constexpr bool is_prime(uint64_t p) {
+    if(p % 2 == 0) return false;
+    
+    const uint64_t m = p >> 1;
+    for(size_t i = 2; i <= m; i ++) {
+        if((p % i) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// finds the smallest prime greater than or equal to p
+inline uint64_t prime_successor(uint64_t p) {
+    if(unlikely(p == 0)) return 0;
+    if(unlikely(p == 2)) return 2;
+    if(p % 2 == 0) ++p; // all primes > 2 are odd
+
+    // linear search - the gap between two primes is hopefully very low
+    // in the worst case, because there must be a prime between p and 2p, this takes p steps
+    while(!is_prime(p)) p += 2;
+    return p;
+}
+
 }
