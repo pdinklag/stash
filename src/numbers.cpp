b9e4b1754a521c118b3054af0087b1ae6c9463a1
Patrick Dinklage <patrick.dinklage@tu-dortmund.de>
2020-02-12 12:43:50 +0100

    Add mode to number generator

--- src/numbers.cpp
+++ src/numbers.cpp
@@ -24,10 +24,13 @@ int main(int argc, char** argv) {
 
     size_t seed = time_nanos();
     cp.add_size_t('s', "seed", seed, "The seed for random generation (default: timestamp).");
-	
+
+    std::string mode = "qred";
+    cp.add_string('m', "mode", mode, "The mode to use ('qred' for quadratic residue, 'add' for addition-based)");
+    
 #ifndef NDEBUG
-	bool check = false;
-	cp.add_flag('c', "check", check, "Check that a permutation is generated (debug).");
+    bool check = false;
+    cp.add_flag('c', "check", check, "Check that a permutation is generated (debug).");
 #endif
 
     if(!cp.process(argc, argv)) {
@@ -40,21 +43,35 @@ int main(int argc, char** argv) {
     }
 
     // generate numbers
-    auto perm = random::permutation(u, seed);
-	
-#ifndef NDEBUG
-	if(check) {
-		std::vector<bool> b(u);
-		for(uint64_t i = 0; i < u; i++) {
-			const uint64_t j = perm(i);
-			assert(!b[j]);
-			b[j] = 1;
-		}
-	}
-#endif
-	
-    for(uint64_t i = 0; i < num; i++) {
-        std::cout << perm(i) << std::endl;
+    if(mode == "qred") {
+        auto perm = random::permutation(u, seed);
+
+    #ifndef NDEBUG
+        if(check) {
+            std::vector<bool> b(u);
+            for(uint64_t i = 0; i < u; i++) {
+                const uint64_t j = perm(i);
+                assert(!b[j]);
+                b[j] = 1;
+            }
+        }
+    #endif
+
+        for(uint64_t i = 0; i < num; i++) {
+            std::cout << perm(i) << std::endl;
+        }
+    } else if(mode == "add") {
+        const auto add_max = u / num;
+        std::default_random_engine gen(seed);
+        std::uniform_int_distribution<uint64_t> init(0, add_max+1);
+        std::uniform_int_distribution<uint64_t> dist(1, add_max+1);
+
+        auto next = init(gen);
+        for(uint64_t i = 0; i < num; i++) {
+            std::cout << next << std::endl;
+            next += dist(gen);
+        }
     }
+    
     return 0;
 }
